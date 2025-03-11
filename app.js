76ff9ecc401195fd8ecdcd92f128a87a0e750ed2
Marcelo-msp <marcelorwx@gmail.com>
2025-03-11 14:37:49 -0300

    Verifica campo vazio e se amigo ja esta na lista

--- app.js
+++ app.js
@@ -3,7 +3,17 @@
 let amigos = [];
 
 function adicionarAmigo(){
-    amigos.push(document.getElementById('amigo').value); //adiciona o nome digitado na lista de amigos
-    document.getElementById('amigo').value = ''; //limpa o campo de texto
-    
+    let amigo = document.getElementById('amigo').value; // Armazena o valor do campo de texto em uma variável
+    if (amigo === '') { // Verifica se o campo de texto está vazio
+        alert('Digite o nome do amigo!'); // Se estiver vazio, exibe um alerta
+        return; // E interrompe a execução da função
+    }
+    if (amigos.includes(amigo)) { // Verifica se o nome digitado já está na lista de amigos
+        alert('Este amigo já foi adicionado!'); // Se estiver, exibe um alerta
+        return; // E interrompe a execução da função
+    } else {
+        amigos.push(amigo); // Adiciona o nome digitado na lista de amigos
+        document.getElementById('amigo').value = ''; // Limpa o campo de texto
+        atualizarListaAmigos(); // Atualiza a lista de amigos na interface
+    }
 }
