2a07717fdbfef8cc0674162b76d6e00b1c9564a9
Marcelo-msp <marcelorwx@gmail.com>
2025-03-11 16:25:12 -0300

    codigo final

--- app.js
+++ app.js
@@ -3,23 +3,23 @@
 let amigos = [];
 
 function adicionarAmigo(){
-    let amigo = document.getElementById('amigo').value; // Armazena o valor do campo de texto em uma variável
+    let amigo = document.getElementById('amigo').value; // Armazena o nome digitado no campo de texto
     if (amigo === '') { // Verifica se o campo de texto está vazio
-        alert('Digite o nome do amigo!'); // Se estiver vazio, exibe um alerta
-        return; // E interrompe a execução da função
+        alert('Digite o nome do amigo!'); // Se estiver vazio, exibe a mensagem solicitando o nome
+        return; // interrompe a função
     }
     if (amigos.includes(amigo)) { // Verifica se o nome digitado já está na lista de amigos
-        alert('Este amigo já foi adicionado!'); // Se estiver, exibe um alerta
-        return; // E interrompe a execução da função
+        alert('Este amigo já foi adicionado!'); // Se estiver na lista, exibe um alerta
+        return; //interrompe a função
     } else {
         amigos.push(amigo); // Adiciona o nome digitado na lista de amigos
         document.getElementById('amigo').value = ''; // Limpa o campo de texto
-        atualizarListaAmigos(); // Atualiza a lista de amigos na interface
+        atualizarListaAmigos(); // Atualiza a lista de amigos
     }
 }
 function atualizarListaAmigos() {
     const listaAmigos = document.getElementById('listaAmigos');
-    listaAmigos.innerHTML = ''; // Limpa a lista antes de adicionar novos elementos
+    listaAmigos.innerHTML = ''; // Limpa a lista antes de adicionar novos amigos
 
     for (let i = 0; i < amigos.length; i++) {
         const li = document.createElement('li');
@@ -29,14 +29,14 @@ function atualizarListaAmigos() {
 }
 
 function sortearAmigo() {
-    if (amigos.length === 0) { // Verifica se há amigos disponíveis
-        alert('Nenhum amigo disponível para sortear!');
+    if (amigos.length === 0) { // Verifica se os disponíveis
+        alert('Nenhum amigo disponível para ser sorteado!');
         return;
     }
 
     const indiceAleatorio = Math.floor(Math.random() * amigos.length); // Gera um índice aleatório
-    const amigoSorteado = amigos[indiceAleatorio]; // Obtém o nome sorteado
+    const amigoSorteado = amigos[indiceAleatorio]; // Obtém o nome sorteado pelo valor do indice
 
     const resultado = document.getElementById('resultado');
-    resultado.innerHTML = `<li>${amigoSorteado}</li>`; // Atualiza o conteúdo do elemento de resultado
+    resultado.innerHTML = `<li>O amigo sorteado foi: ""${amigoSorteado}""</li>`; // Exibe na tela o nome do amigo sorteado
 }
